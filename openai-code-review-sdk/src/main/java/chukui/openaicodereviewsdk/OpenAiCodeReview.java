7de61447d5a1787b31507e0ee7781993254a5113
chukui <3324803357@qq.com>
2026-05-26 00:56:48 +0800

    feat: 新增微信模板消息推送
    - 新增 WXAccessTokenUtils 获取微信 access_token
    - OpenAiCodeReview 主流程新增第4步 pushMessage()
    - 新增 sendPostRequest() HTTP 工具方法
    - Message 配置用户实际的 touser 和 template_id

--- openai-code-review-sdk/src/main/java/chukui/openaicodereviewsdk/OpenAiCodeReview.java
+++ openai-code-review-sdk/src/main/java/chukui/openaicodereviewsdk/OpenAiCodeReview.java
@@ -5,7 +5,9 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import chukui.openaicodereviewsdk.domain.model.ChatCompletionRequest;
 import chukui.openaicodereviewsdk.domain.model.ChatCompletionSyncResponse;
+import chukui.openaicodereviewsdk.domain.model.Message;
 import chukui.openaicodereviewsdk.domain.model.Model;
+import chukui.openaicodereviewsdk.types.utils.WXAccessTokenUtils;
 
 import java.io.*;
 import java.net.HttpURLConnection;
@@ -15,6 +17,7 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
+import java.util.Scanner;
 
 public class OpenAiCodeReview {
 
@@ -52,6 +55,10 @@ public class OpenAiCodeReview {
         // 3. 写入评审日志
         String logUrl = writeLog(token, log);
         System.out.println("writeLog：" + logUrl);
+
+        // 4. 消息通知
+        System.out.println("pushMessage：" + logUrl);
+        pushMessage(logUrl);
     }
 
     private static String codeReview(String diffCode) throws Exception {
@@ -72,7 +79,7 @@ public class OpenAiCodeReview {
             private static final long serialVersionUID = -7988151926241837899L;
 
             {
-                add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言请，请您根据git diff记录，对代码做出评审。代码如下:"));
+                add(new ChatCompletionRequest.Prompt("user", "你是一个高级编程架构师，精通各类场景方案、架构设计和编程语言，请您根据git diff记录，对代码做出评审。代码如下:"));
                 add(new ChatCompletionRequest.Prompt("user", diffCode));
             }
         });
@@ -100,6 +107,42 @@ public class OpenAiCodeReview {
 
         ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
         return response.getChoices().get(0).getMessage().getContent();
+    }
+
+    private static void pushMessage(String logUrl) {
+        String accessToken = WXAccessTokenUtils.getAccessToken();
+        System.out.println(accessToken);
+
+        Message message = new Message();
+        message.put("project", "big-market");
+        message.put("review", logUrl);
+        message.setUrl(logUrl);
+
+        String url = String.format("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=%s", accessToken);
+        sendPostRequest(url, JSON.toJSONString(message));
+    }
+
+    private static void sendPostRequest(String urlString, String jsonBody) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonBody.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            try (Scanner scanner = new Scanner(conn.getInputStream(), StandardCharsets.UTF_8.name())) {
+                String response = scanner.useDelimiter("\\A").next();
+                System.out.println(response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private static String writeLog(String token, String log) throws Exception {
--- openai-code-review-sdk/src/test/java/chukui/openaicodereviewsdk/ApiTest.java
+++ openai-code-review-sdk/src/test/java/chukui/openaicodereviewsdk/ApiTest.java
@@ -2,6 +2,7 @@ package chukui.openaicodereviewsdk;
 
 import com.alibaba.fastjson2.JSON;
 import chukui.openaicodereviewsdk.domain.model.ChatCompletionSyncResponse;
+import chukui.openaicodereviewsdk.types.utils.WXAccessTokenUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -10,6 +11,9 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 import org.junit.jupiter.api.Test;
 
 public class ApiTest {
@@ -65,6 +69,89 @@ public class ApiTest {
 
         ChatCompletionSyncResponse response = JSON.parseObject(content.toString(), ChatCompletionSyncResponse.class);
         System.out.println(response.getChoices().get(0).getMessage().getContent());
+    }
+
+    @Test
+    public void test_wx() {
+        String accessToken = WXAccessTokenUtils.getAccessToken();
+        System.out.println(accessToken);
+
+        Message message = new Message();
+        message.put("project","big-market");
+        message.put("review","feat: 新加功能");
+
+        String url = String.format("https://api.weixin.qq.com/cgi-bin/message/template/send?access_token=%s", accessToken);
+        sendPostRequest(url, JSON.toJSONString(message));
+    }
+
+    private static void sendPostRequest(String urlString, String jsonBody) {
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; utf-8");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setDoOutput(true);
+
+            try (OutputStream os = conn.getOutputStream()) {
+                byte[] input = jsonBody.getBytes(StandardCharsets.UTF_8);
+                os.write(input, 0, input.length);
+            }
+
+            try (Scanner scanner = new Scanner(conn.getInputStream(), StandardCharsets.UTF_8.name())) {
+                String response = scanner.useDelimiter("\\A").next();
+                System.out.println(response);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static class Message {
+        private String touser = "on86i2EdZXNK7fSPpTJKxKeeny8E";
+        private String template_id = "rZclTXb9XDWG--aRAf0tnXpQi2xOyfs7tLBI_TQrVYQ";
+        private String url = "https://github.com/chukui2005/openai-code-review-log/blob/master/2024-07-27/Wzpxr6j1JY9k.md";
+        private Map<String, Map<String, String>> data = new HashMap<>();
+
+        public void put(String key, String value) {
+            data.put(key, new HashMap<String, String>() {
+                {
+                    put("value", value);
+                }
+            });
+        }
+
+        public String getTouser() {
+            return touser;
+        }
+
+        public void setTouser(String touser) {
+            this.touser = touser;
+        }
+
+        public String getTemplate_id() {
+            return template_id;
+        }
+
+        public void setTemplate_id(String template_id) {
+            this.template_id = template_id;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+            this.url = url;
+        }
+
+        public Map<String, Map<String, String>> getData() {
+            return data;
+        }
+
+        public void setData(Map<String, Map<String, String>> data) {
+            this.data = data;
+        }
     }
 
 }
