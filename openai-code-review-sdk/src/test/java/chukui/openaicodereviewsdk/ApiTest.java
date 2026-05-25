5f855fec98680368d3ae11bc73c024df6e03edaf
chukui <3324803357@qq.com>
2026-05-25 14:40:35 +0800

    feat: init project
    初始化 Maven 多模块工程，实现 git diff 代码检出

---
+++ openai-code-review-sdk/src/test/java/chukui/openaicodereviewsdk/ApiTest.java
@@ -0,0 +1,15 @@
+package chukui.openaicodereviewsdk;
+
+/**
+ * @Author: chukui
+ * @CreateTime: 2026-05-24  21:08
+ * @Description: TODO
+ * @Version: 1.0
+ */
+public class ApiTest {
+
+    public static void main(String[] args) {
+
+    }
+
+}
